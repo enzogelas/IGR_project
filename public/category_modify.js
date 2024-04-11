bb1518dc10f1d1795c2319a06fda2d9b81a8c8a5
enzo.gelas <enzo.gelas@telecom-paris.fr>
2024-04-11 12:23:37 +0200

    Everything implemented, no new css

---
+++ public/category_modify.js
@@ -0,0 +1,54 @@
+const xhttp = new XMLHttpRequest();
+
+function cancel() {
+    
+    window.location.href = "../";
+
+}
+
+function displayCurrentCategory () {
+
+    console.log("Fetching current category");
+
+    xhttp.onreadystatechange = function () {
+        if (this.status == 200 && this.readyState == 4) {
+            let category = JSON.parse(this.responseText);
+
+            let categoryName = document.getElementById("name");
+            let categoryColor = document.getElementById("color");
+
+            categoryName.textContent = category.name;
+            categoryColor.value = category.color;
+        }
+    }
+
+    xhttp.open("GET", "../currentCategory");
+    xhttp.send();
+    
+}
+
+function validateModification() {
+    let categoryName = document.getElementById("name").textContent;
+    let categoryColor = document.getElementById("color").value;
+
+    const newCategory = {
+        name: categoryName,
+        color: categoryColor
+    };
+
+    console.log(newCategory);
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+
+    }
+    
+    xhttp.open("POST", "../modify_category");
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(newCategory));
+
+    window.location.href = "../";
+}
+
+displayCurrentCategory();
